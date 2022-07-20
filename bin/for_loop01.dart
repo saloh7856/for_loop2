4938a53a9de6491240705fac6345924969a5dca9
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-20 14:49:08 +0500

    01

--- bin/for_loop01.dart
+++ bin/for_loop01.dart
@@ -1,4 +1,4 @@
-int func(List numbers) {
+
   /*
 Given the list of numbers, return sum of the numbers in the list
 Args:
@@ -6,7 +6,14 @@ Args:
 Return: 
   Int: answer
 */
-  return 0;
+int func(List<int> x){
+    int s=0;
+    int l=x.length;
+    for(int i=0; i<l; i+=1){
+            s+=x[i];
+    }
+    return s;
 }
-
-void main() {}
+void main(){
+    print(func(<int>[1,2,3,4,5,6]));
+}
