ec125c599f22241850032f7d50b2c6f7ca1887c2
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-20 14:53:57 +0500

    03

--- bin/for_loop03.dart
+++ bin/for_loop03.dart
@@ -1,4 +1,4 @@
-int func(List<int> numbers) {
+
 /*
    Given a list of numbers, return the sum of the numbers at odd index in the list
 Args:
@@ -6,7 +6,17 @@ Args:
 Return: 
   Int: answer
 */
-  return 0;
+  int func(List<int> x){
+    int s=0;
+    int l=x.length;
+    for(int i=1; i<l; i+=2){
+            s+=x[i];
+    }
+    return s;
+}
+void main(){
+    print(func(<int>[1,2,3,4,5,6]));
 }
 
-void main() {}
+
+
